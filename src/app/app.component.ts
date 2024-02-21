17c9fd1eb22663f020e3a6a91b9e6f0c1d047a92
bsw <bsw@wstbrk.dev>
2024-02-21 15:37:49 -0600

    Added JSON to XML code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ExportService } from './export.service';
 import * as xml2js from 'xml2js';
 import * as ExcelJS from 'exceljs';
+import X2JS from 'x2js';
 
 @Component({
   selector: 'app-root',
@@ -21,6 +22,10 @@ export class AppComponent {
   authKey: String = '';
 
   constructor(public exportServ: ExportService) { }
+
+  export() {
+    //console.log(this.exportServ.exportExcel(this.file))
+  }
 
   getKey(val: String) {
     this.authKey = val;
@@ -43,18 +48,26 @@ export class AppComponent {
     }
   }
 
-  export() {
-    //console.log(this.exportServ.exportExcel(this.file))
+  convertJSONToXML(jsonData: any) {
+    var x2js = new X2JS();
+    var document = x2js.xml2js(jsonData);
+
+    console.log(document);
+
+    var xmlData = x2js.js2xml(document);
+    console.log(xmlData);
   }
-/*
-  convertJSON2XLSX = (jsonData: any) => {
-    let workbook = new ExcelJS.Workbook();
-    var request = new XMLHttpRequest();
-    request.open("GET", "/assets/csv_templates/VERIFI-Import-Data.xlsx", true);
-    request.responseType = "blob";
-    request.onload = () => {
-      workbook.xlsx.load(request.response).then(() => {
-      });
-    };
-  }*/
+
+
+  /*
+    convertJSON2XLSX = (jsonData: any) => {
+      let workbook = new ExcelJS.Workbook();
+      var request = new XMLHttpRequest();
+      request.open("GET", "/assets/csv_templates/VERIFI-Import-Data.xlsx", true);
+      request.responseType = "blob";
+      request.onload = () => {
+        workbook.xlsx.load(request.response).then(() => {
+        });
+      };
+    }*/
 };
