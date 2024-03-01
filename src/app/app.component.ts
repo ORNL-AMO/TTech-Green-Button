705ca5e96e59fa65706e6139226441340f9fc90a
bsw <bsw@wstbrk.dev>
2024-02-29 19:52:34 -0600

    Added more code that will help us go from JSON to TS objects (and then to XLSX)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,65 @@ import { RouterOutlet } from '@angular/router';
 import { LogoButtonComponent } from './logo-button/logo-button.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ExportService } from './export.service';
-import * as xml2js from 'xml2js';
 import * as ExcelJS from 'exceljs';
 import X2JS from 'x2js';
+
+
+//These are special tools which will help us later...
+//Used for converting JSON to TS Objects
+export interface Root {
+  uid: string
+  meter_uid: string
+  authorization_uid: string
+  created: string
+  updated: string
+  utility: string
+  blocks: string[]
+  base: Base
+  sources: Source[]
+  line_items: LineItem[]
+  tiers: Tier[]
+}
+
+export interface Base {
+  service_identifier: string
+  service_tariff: string
+  service_class: string
+  service_address: string
+  meter_numbers: string[]
+  billing_contact: string
+  billing_address: string
+  billing_account: string
+  bill_start_date: string
+  bill_end_date: string
+  bill_total_kwh: number
+  bill_total_unit: string
+  bill_total_volume: number
+  bill_total_cost: number
+}
+
+export interface Source {
+  type: string
+  raw_url: string
+}
+
+export interface LineItem {
+  cost: number
+  end: string
+  name: string
+  rate?: number
+  start: string
+  unit?: string
+  volume?: number
+}
+
+export interface Tier {
+  name: string
+  level: number
+  cost: number
+  volume: number
+  unit: string
+}
 
 @Component({
   selector: 'app-root',
@@ -66,8 +122,10 @@ export class AppComponent {
   }
 
   convertJSONToExcel(jsonData: any) {
+    //Create a workbook
     const workbook = new ExcelJS.Workbook();
 
+    //Add worksheets to the workbook following the VERIFI format
     const facilitiesSheet = workbook.addWorksheet('Facilities');
     const metersutilitiesSheet = workbook.addWorksheet('Meters-Utilities');
     const electricitySheet = workbook.addWorksheet('Electricity');
@@ -77,6 +135,8 @@ export class AppComponent {
     const otheremmissionsSheet = workbook.addWorksheet('Other Utility - Emission');
     const predictorsSheet = workbook.addWorksheet('Predictors');
 
+    //The below sheets are not accurate to the JSON at the moment
+    //Facilities
     facilitiesSheet.columns = [
       { header: 'Facility Name', key: "Facility Name", width: 20 },
       { header: 'Address', key: "Address", width: 20 },
@@ -90,40 +150,40 @@ export class AppComponent {
       { header: 'Contact Email', key: "Contact Email", width: 20 },
     ];
 
-    //These keys are not accurate to the JSON at the moment
+
     metersutilitiesSheet.columns = [
       { header: 'Facility Name', key: "Facility Name", width: 20 },
-      { header: 'Meter Number (unique)', key: "Facility ID", width: 20 },
-      { header: 'Source', key: "Facility ID", width: 20 },
-      { header: 'Scope', key: "Facility ID", width: 20 },
-      { header: 'Meter Name (Display)', key: "Facility ID", width: 20 },
-      { header: 'Meter Group', key: "Facility ID", width: 20 },
-      { header: 'Calendarize Data?', key: "Facility ID", width: 20 },
-      { header: 'Phase or Vehicle', key: "Facility ID", width: 20 },
-      { header: 'Fuel or Emission', key: "Facility ID", width: 20 },
-      { header: 'Collection Unit', key: "Facility ID", width: 20 },
-      { header: 'Energy Unit', key: "Facility ID", width: 20 },
-      { header: 'Distance Unit', key: "Facility ID", width: 20 },
-      { header: 'Estimation Method', key: "Facility ID", width: 20 },
-      { header: 'Heat Capacity or Fuel Efficiency', key: "Facility ID", width: 20 },
-      { header: 'Include In Energy', key: "Facility ID", width: 20 },
-      { header: 'Site To Source', key: "Facility ID", width: 20 },
-      { header: 'Agreement Type', key: "Facility ID", width: 20 },
-      { header: 'Retain RECS', key: "Facility ID", width: 20 },
-      { header: 'Account Number', key: "Facility ID", width: 20 },
-      { header: 'Utility Supplier', key: "Facility ID", width: 20 },
-      { header: 'Notes', key: "Facility ID", width: 20 },
-      { header: 'Building / Location', key: "Facility ID", width: 20 }
+      { header: 'Meter Number (unique)', key: "Meter Number (unique)", width: 20 },
+      { header: 'Source', key: "Source", width: 20 },
+      { header: 'Scope', key: "Scope", width: 20 },
+      { header: 'Meter Name (Display)', key: "Meter Name (Display)", width: 20 },
+      { header: 'Meter Group', key: "Meter Group", width: 20 },
+      { header: 'Calendarize Data?', key: "Calendarize Data?", width: 20 },
+      { header: 'Phase or Vehicle', key: "Phase or Vehicle", width: 20 },
+      { header: 'Fuel or Emission', key: "Fuel or Emission", width: 20 },
+      { header: 'Collection Unit', key: "Collection Unit", width: 20 },
+      { header: 'Energy Unit', key: "Energy Unit", width: 20 },
+      { header: 'Distance Unit', key: "Distance Unit", width: 20 },
+      { header: 'Estimation Method', key: "Estimation Method", width: 20 },
+      { header: 'Heat Capacity or Fuel Efficiency', key: "Heat Capacity or Fuel Efficiency", width: 20 },
+      { header: 'Include In Energy', key: "Include In Energy", width: 20 },
+      { header: 'Site To Source', key: "Site To Source", width: 20 },
+      { header: 'Agreement Type', key: "Agreement Type", width: 20 },
+      { header: 'Retain RECS', key: "Retain RECS", width: 20 },
+      { header: 'Account Number', key: "Account Number", width: 20 },
+      { header: 'Utility Supplier', key: "Utility Supplier", width: 20 },
+      { header: 'Notes', key: "Notes", width: 20 },
+      { header: 'Building / Location', key: "Building / Location", width: 20 }
     ];
 
     electricitySheet.columns = [
       { header: 'Meter Number', key: "Meter Number", width: 20 },
-      { header: 'Read Date', key: "Facility ID", width: 20 },
-      { header: 'Total Consuption', key: "Facility ID", width: 20 },
-      { header: 'Total Real Demand', key: "Facility ID", width: 20 },
-      { header: 'Total Billed Demand', key: "Facility ID", width: 20 },
-      { header: 'Total Cost', key: "Facility ID", width: 20 },
-      { header: 'Non-energy Charge', key: "Facility ID", width: 20 },
+      { header: 'Read Date', key: "Read Date", width: 20 },
+      { header: 'Total Consuption', key: "Total Consuption", width: 20 },
+      { header: 'Total Real Demand', key: "Total Real Demand", width: 20 },
+      { header: 'Total Billed Demand', key: "Total Billed Demand", width: 20 },
+      { header: 'Total Cost', key: "Total Cost", width: 20 },
+      { header: 'Non-energy Charge', key: "Non-energy Charge", width: 20 },
       { header: 'Block 1 Consuption', key: "Block 1 Consuption", width: 20 },
       { header: 'Block 1 Consuption Charge', key: "Block 1 Consuption Charge", width: 20 },
       { header: 'Block 2 Consuption', key: "Block 2 Consuption", width: 20 },
@@ -269,6 +329,8 @@ export class AppComponent {
       { header: 'Notes', key: "Facility ID", width: 20 },
       { header: 'Building / Location', key: "Facility ID", width: 20 }
     ];
+
+
 
     workbook.xlsx.writeBuffer()
       .then((buffer: BlobPart) => {
