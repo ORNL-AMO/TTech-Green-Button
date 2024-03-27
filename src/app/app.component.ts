c577b11ba1b701a84f1194e66027027674546e9c
Connor Powell <connorallenpowell@gmail.com>
2024-03-27 14:55:28 -0500

    removed useless code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -38,122 +38,6 @@ export class AppComponent implements OnInit {
   authForm: any;
   meterForm: any;
   billsForm: any;
-  billingData: any = {
-    "uid": "12382",
-    "meter_uid": "58",
-    "authorization_uid": "23",
-    "created": "2015-03-19T20:50:41.206253+00:00",
-    "updated": "2015-03-19T20:50:41.206253+00:00",
-    "utility": "DEMO",
-    "blocks": [
-      "base",
-      "sources",
-      "line_items",
-      "tiers"
-    ],
-    "base": {
-      "service_identifier": "1234567890",
-      "service_tariff": "E1 XB Residential Service",
-      "service_class": "res-electric",
-      "service_address": "123 Main St #100, Anytown, CA 94612",
-      "meter_numbers": [
-        "A987654321"
-      ],
-      "billing_contact": "ACME, INC.",
-      "billing_address": "123 Main St #100, Anytown, CA 94612",
-      "billing_account": "100-200-33333",
-      "bill_start_date": "2015-01-07T00:00:00.000000-08:00",
-      "bill_end_date": "2015-02-05T00:00:00.000000-08:00",
-      "bill_total_kwh": 837.0,
-      "bill_total_unit": "kwh",
-      "bill_total_volume": 837.0,
-      "bill_total_cost": 194.78
-    },
-    "sources": [
-      {
-        "type": "pdf",
-        "raw_url": "https://utilityapi.com/api/v2/files/abc123abc123abc123"
-      }
-    ],
-    "line_items": [
-      {
-        "cost": 52.88,
-        "end": "2015-02-05T08:00:00.000000+00:00",
-        "name": "Tier 1 Usage",
-        "rate": 0.1617,
-        "start": "2015-01-07T08:00:00.000000+00:00",
-        "unit": "kwh",
-        "volume": 327.0
-      },
-      {
-        "cost": 18.14,
-        "end": "2015-02-05T08:00:00.000000+00:00",
-        "name": "Tier 2 Usage",
-        "rate": 0.18491,
-        "start": "2015-01-07T08:00:00.000000+00:00",
-        "unit": "kwh",
-        "volume": 98.1
-      },
-      {
-        "cost": 62.54,
-        "end": "2015-02-05T08:00:00.000000+00:00",
-        "name": "Tier 3 Usage",
-        "rate": 0.27322,
-        "start": "2015-01-07T08:00:00.000000+00:00",
-        "unit": "kwh",
-        "volume": 228.9
-      },
-      {
-        "cost": 60.98,
-        "end": "2015-02-05T08:00:00.000000+00:00",
-        "name": "Tier 4 Usage",
-        "rate": 0.33322,
-        "start": "2015-01-07T08:00:00.000000+00:00",
-        "unit": "kwh",
-        "volume": 183.0
-      },
-      {
-        "cost": 0.24,
-        "end": "2015-02-05T08:00:00.000000+00:00",
-        "name": "Energy Commission Tax",
-        "rate": null,
-        "start": "2015-01-07T08:00:00.000000+00:00",
-        "unit": null,
-        "volume": null
-      }
-    ],
-    "tiers": [
-      {
-        "name": "Tier 1 Usage",
-        "level": 1,
-        "cost": 52.88,
-        "volume": 327.0,
-        "unit": "kwh"
-      },
-      {
-        "name": "Tier 2 Usage",
-        "level": 2,
-        "cost": 18.14,
-        "volume": 98.1,
-        "unit": "kwh"
-      },
-      {
-        "name": "Tier 3 Usage",
-        "level": 3,
-        "cost": 62.54,
-        "volume": 228.9,
-        "unit": "kwh"
-      },
-      {
-        "name": "Tier 4 Usage",
-        "level": 4,
-        "cost": 60.98,
-        "volume": 183.0,
-        "unit": "kwh"
-      }
-    ]
-  }
-
 
   constructor(public exportServ: ExportService, public apiServ: ApiHttpService, public http: HttpClient) {
     console.log(Constants.API_ENDPOINT);
@@ -249,39 +133,39 @@ async apiNew(){
     });
     // this.authForm = await this.apiServ.get(Constants.API_ENDPOINT1 + 'authorizations/?referrals=' + referral.referral + '&' + this.accessToken + '&include=meters')
 
-    console.log(TestauthForm);
-    console.log(TestauthForm.authorizations[0].meters.meters[0].uid)
-    let meterID = TestauthForm.authorizations[0].meters.meters[0].uid
-    const data1: any = await this.apiServ.post(Constants.API_ENDPOINT1 + 'meters/historical-collection?' + this.accessToken, '{"meters": ['+ TestauthForm.authorizations[0].meters.meters[0].uid +']}');
-    //    'https://utilityapi.com/api/v2/meters/historical-collection'
-    console.log(data1);
-    let ticks = 1000
-    var pollMeter: any = await new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(this.apiServ.get(Constants.API_ENDPOINT1 + 'meters/' + meterID + '?' + this.accessToken));
-      }, ticks); // 25000 milliseconds = 25 seconds
-    });
-    while (pollMeter.status == "pending"){
-      ticks += 5000
-        pollMeter = await new Promise((resolve) => {
-        setTimeout(() => {
-          resolve(this.apiServ.get(Constants.API_ENDPOINT1 + 'meters/' + meterID + '?' + this.accessToken));
-        }, ticks); // 25000 milliseconds = 25 seconds
-      });
-    }
-    console.log(pollMeter)
-    const data2: any = await this.apiServ.get(Constants.API_ENDPOINT1 + 'bills?meters='+ meterID +'&' + this.accessToken);
-    console.log(data2)
-    // this.authForm = await this.apiServ.get(Constants.API_ENDPOINT1 + 'authorizations/' + authToken + '?referrals=' + referral.referral + '&' + this.accessToken)
-    // this.meterForm = await this.apiServ.get(Constants.API_ENDPOINT1 + 'meters?authorizations='+ authToken + '&' + this.accessToken)
-    //this.billsForm = await this.apiServ.get(Constants.API_ENDPOINT1 + 'bills?authorizations=' + authToken + '&' + this.accessToken)
+    // console.log(TestauthForm);
+    // console.log(TestauthForm.authorizations[0].meters.meters[0].uid)
+    // let meterID = TestauthForm.authorizations[0].meters.meters[0].uid
+    // const data1: any = await this.apiServ.post(Constants.API_ENDPOINT1 + 'meters/historical-collection?' + this.accessToken, '{"meters": ['+ TestauthForm.authorizations[0].meters.meters[0].uid +']}');
+    // //    'https://utilityapi.com/api/v2/meters/historical-collection'
+    // console.log(data1);
+    // let ticks = 1000
+    // var pollMeter: any = await new Promise((resolve) => {
+    //   setTimeout(() => {
+    //     resolve(this.apiServ.get(Constants.API_ENDPOINT1 + 'meters/' + meterID + '?' + this.accessToken));
+    //   }, ticks); // 25000 milliseconds = 25 seconds
+    // });
+    // while (pollMeter.status == "pending"){
+    //   ticks += 5000
+    //     pollMeter = await new Promise((resolve) => {
+    //     setTimeout(() => {
+    //       resolve(this.apiServ.get(Constants.API_ENDPOINT1 + 'meters/' + meterID + '?' + this.accessToken));
+    //     }, ticks); // 25000 milliseconds = 25 seconds
+    //   });
+    // }
+    // console.log(pollMeter)
+    // const data2: any = await this.apiServ.get(Constants.API_ENDPOINT1 + 'bills?meters='+ meterID +'&' + this.accessToken);
+    // console.log(data2)
+    // // this.authForm = await this.apiServ.get(Constants.API_ENDPOINT1 + 'authorizations/' + authToken + '?referrals=' + referral.referral + '&' + this.accessToken)
+    // // this.meterForm = await this.apiServ.get(Constants.API_ENDPOINT1 + 'meters?authorizations='+ authToken + '&' + this.accessToken)
+    // //this.billsForm = await this.apiServ.get(Constants.API_ENDPOINT1 + 'bills?authorizations=' + authToken + '&' + this.accessToken)
 
-    //Code to handle successful response
-    // console.log(this.authForm);
-    // console.log(this.meterForm);
-    // console.log(this.billsForm);
-    //ParseService.convertJSONToExcel(this.billingData, this.meterForm)
-    //this.exportServ.exportJSON(data2)
+    // //Code to handle successful response
+    // // console.log(this.authForm);
+    // // console.log(this.meterForm);
+    // // console.log(this.billsForm);
+    // //ParseService.convertJSONToExcel(this.billingData, this.meterForm)
+    // //this.exportServ.exportJSON(data2)
     
   } catch (error) {
     console.log(error)
@@ -292,6 +176,7 @@ async apiNew(){
   async apiCall (authToken: string,startDate?:string,endDate?:string) {
     //for returning users that have their auth token
     console.log (authToken)
+    //mark token 436742
     //437638
 
     try {
